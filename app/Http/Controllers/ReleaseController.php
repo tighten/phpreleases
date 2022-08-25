cbb32f8b43396d2f9e8cab6832bccee4e2b619b1
faxblaster <alison.l.kirk@gmail.com>
2022-08-25 14:21:34 -0400

    add validation for request params

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Release;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ReleaseController
 {
@@ -17,6 +19,12 @@ class ReleaseController
 
     public function minimumSupported(string $supportType = 'active')
     {
+        request()
+            ->merge(['supportType' => $supportType])
+            ->validate([
+                'supportType' => 'nullable|' . Rule::in(['active', 'security']),
+            ]);
+
         return response()->json(
             Release::where("{$supportType}_support_until", '>', Carbon::now())
                 ->orderBy('major')
@@ -40,11 +48,21 @@ class ReleaseController
     {
         $release = explode('.', $request->release);
 
+        $validator = Validator::make([
+            'major' => $release[0],
+            'minor' => $release[1] ?? null,
+            'release' => $release[2] ?? null,
+        ], [
+            'major' => ['required', 'regex:/^[\',"]?[5-9]|1[0-1][\',"]?$/'],
+            'minor' => ['nullable', 'regex:/^[\',"]?[0-9]{1,3}[\',"]?$/'],
+            'release' => ['nullable', 'regex:/^[\',"]?[0-9]{1,3}[\',"]?$/'],
+        ])->validate();
+
         if (count($release) === 3) {
             $provided = Release::firstWhere([
-                'major' => $release[0],
-                'minor' => $release[1],
-                'release' => $release[2],
+                'major' => $validator['major'],
+                'minor' => $validator['minor'],
+                'release' => $validator['release'],
             ]);
 
             return response()->json([
@@ -58,12 +76,12 @@ class ReleaseController
 
         return response()->json(
             Release::query()
-                ->when(array_key_exists(1, $release), function ($query) use ($release) {
-                    $query->where('major', $release[0])
-                        ->where('minor', $release[1]);
+                ->when(array_key_exists(1, $release), function ($query) use ($validator) {
+                    $query->where('major', $validator['major'])
+                        ->where('minor', $validator['minor']);
                 })
-                ->when(array_key_exists(0, $release), function ($query) use ($release) {
-                    $query->where('major', $release[0]);
+                ->when(array_key_exists(0, $release), function ($query) use ($validator) {
+                    $query->where('major', $validator['major']);
                 })
                 ->orderByDesc('tagged_at')
                 ->get()
