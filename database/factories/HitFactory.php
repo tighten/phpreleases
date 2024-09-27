0ddf832ccde77af5100d5df76efbf064c6e96b9a
Shift <shift@laravelshift.com>
2024-09-27 18:35:28 +0000

    Use Faker methods
    Accessing Faker properties was deprecated in Faker 1.14.

--- database/factories/HitFactory.php
+++ database/factories/HitFactory.php
@@ -10,9 +10,9 @@ class HitFactory extends Factory
     {
         return [
             'endpoint' => '/api/releases/' . random_int(5, 9) . '.' . random_int(0, 12) . random_int(0, 25),
-            'user_agent' => $this->faker->userAgent,
-            'referer' => $this->faker->url,
-            'ip' => $this->faker->ipv4,
+            'user_agent' => $this->faker->userAgent(),
+            'referer' => $this->faker->url(),
+            'ip' => $this->faker->ipv4(),
         ];
     }
 }
