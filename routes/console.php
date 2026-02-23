ca62821301116931f92da09d9595a4ae82f72e76
Shift <shift@laravelshift.com>
2026-02-23 17:51:13 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Console\Commands\SendStatsToSlack;
+use App\Console\Commands\SyncPhpReleaseGraphic;
+use App\Console\Commands\SyncPhpReleases;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command(SyncPhpReleases::class)->twiceDaily();
+Schedule::command(SyncPhpReleaseGraphic::class)->daily();
+Schedule::command(SendStatsToSlack::class)->weeklyOn(5, '8:00');
