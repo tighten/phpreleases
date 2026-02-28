a753acf713d7523decaad21063c852dea34133bc
Shift <shift@laravelshift.com>
2026-02-28 19:45:22 +0000

    Configure JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,22 @@
-const colors = require('tailwindcss/colors');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  corePlugins: {
-      overflow: true
-  },
-  purge: [
-    './resources/**/*.blade.php',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-      colors: {
-          'gray-50': '#FAFAFA',
-          'gray-100': '#F3F4F6',
-          'gray-200': '#E4E4E7',
-          'gray-500': '#71717A',
-          'gray-700': '#2E3036',
-          'white' : colors.white,
-          'black' : colors.black,
-          'indigo' : colors.indigo,
-          'teal' : colors.teal,
-      }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./resources/**/*.blade.php'],
+    theme: {
+        colors: {
+            'gray-50': '#FAFAFA',
+            'gray-100': '#F3F4F6',
+            'gray-200': '#E4E4E7',
+            'gray-500': '#71717A',
+            'gray-700': '#2E3036',
+            white: colors.white,
+            black: colors.black,
+            indigo: colors.indigo,
+            teal: colors.teal,
+        },
+    },
+    plugins: [],
+    corePlugins: {
+        overflow: true,
+    },
 }
